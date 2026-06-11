171f94473511659d4bcfdef2d1284ea6f97685e9
ViaBorders Developer <viaborders@example.org>
2026-06-11 16:19:47 +0200

    REFACT: Resolve code quality issues.
    Resolve project's initial issues raised by:
    * Checkstyle
    * SonarQube for IDE
    * IntelliJ IDEA Code Inspection

--- src/main/java/org/viaborders/Application.java
+++ src/main/java/org/viaborders/Application.java
@@ -3,10 +3,20 @@ package org.viaborders;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * Java and Spring Boot main application class.
+ */
 @SpringBootApplication
 public class Application {
 
-  public static void main(String[] args) {
+  /**
+   * Spring Boot application starts here.
+   *
+   * @param args Java command-line arguments.
+   */
+
+  static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }
 
