59007a2616cc2e8ede90d75cd51ede4725804126
ViaBorders Developer <viaborders@example.org>
2026-06-11 16:09:52 +0200

    REFACT: Rename main application class.

--- src/main/java/org/viaborders/Application.java
+++ src/main/java/org/viaborders/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class XExampleViabordersApplication {
+public class Application {
 
   public static void main(String[] args) {
-    SpringApplication.run(XExampleViabordersApplication.class, args);
+    SpringApplication.run(Application.class, args);
   }
 
 }
