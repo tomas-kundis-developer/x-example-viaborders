3cae08709de7db5660ba7c36cbd2544eba988c3b
ViaBorders Developer <viaborders@example.org>
2026-06-11 14:50:36 +0200

    Initial commit.

---
+++ src/main/java/org/viaborders/XExampleViabordersApplication.java
@@ -0,0 +1,13 @@
+package org.viaborders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XExampleViabordersApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(XExampleViabordersApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/viaborders/XExampleViabordersApplicationTests.java
@@ -0,0 +1,13 @@
+package org.viaborders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XExampleViabordersApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
